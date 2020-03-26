85176473699e1e5b86feeca46f1d01b94f937379
Vitalii Martyniak <vitalldh@gmail.com>
2020-03-26 07:45:51 +0200

    Rename contract status to `pending.winner-signing`

--- src/openprocurement/api/models.py
+++ src/openprocurement/api/models.py
@@ -679,7 +679,7 @@ class Contract(Model):
     description = StringType()  # Contract description
     description_en = StringType()
     description_ru = StringType()
-    status = StringType(choices=["pending", "pending.winnerSigning", "terminated", "active", "cancelled"], default="pending")
+    status = StringType(choices=["pending", "pending.winner-signing", "terminated", "active", "cancelled"], default="pending")
     period = ModelType(Period)
     value = ModelType(Value)
     dateSigned = IsoDateTimeType()
