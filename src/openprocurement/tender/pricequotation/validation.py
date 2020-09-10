3db8057425ac7f9095a757e8085f007bbfcfa091
yshalenyk <yshalenyk@gmail.com>
2020-09-10 14:34:16 +0300

    Fix max_value validation

--- src/openprocurement/tender/pricequotation/validation.py
+++ src/openprocurement/tender/pricequotation/validation.py
@@ -196,7 +196,7 @@ def matches(criteria, response):
                 )
             )
     if not min_value and max_value:
-        if value < min_value:
+        if value > max_value:
             raise ValidationError(
                 u'Value {} is higher then required {} in reqirement {}'.format(
                     value,
